abc6a99da8ce0e561806b9e2329225e5584d5556
Clicky Coyote <clicky@coyote.fm>
2025-04-18 19:15:47 -0500

    add components

---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from './ui/card'
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>Clicky Coyote</CardTitle>
+      <CardDescription>Social Links</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p><a href="https://bsky.app/clicky.coyote.fm">BlueSky</a></p>
+    </CardContent>
+    <CardFooter>
+    </CardFooter>
+  </Card>
+</template>
+
+<style scoped>
+</style>
