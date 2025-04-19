3bf9fcc83962698e4740455f1ed441f94aace884
Clicky Coyote <clicky@coyote.fm>
2025-04-19 11:50:26 -0500

    Fix title font size

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 <template>
   <Card class="bg-sky-400 my-1 max-md:mx-1 md:mx-32 md:w-xl md:pr-auto gap-0">
     <CardHeader>
-      <CardTitle class="text-5xl font-extralight text-sky-900"><img src="../assets/profile-pic.png"
+      <CardTitle class="text-4xl font-extralight text-sky-900"><img src="../assets/profile-pic.png"
                                                                     class="h-16 mr-4 inline">Clicky
         Coyote
       </CardTitle>
