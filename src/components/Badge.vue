1ac664ee0e1127eca466abc9bcc63f8a79d8d2a7
Clicky Coyote <clicky@coyote.fm>
2025-09-01 13:39:24 -0500

    added more badges

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -3,7 +3,7 @@ const { randomBadge } = defineProps<{ randomBadge?: Boolean }>()
 
 const getBadge = () => {
   if (randomBadge) {
-    const badges = ['badge', 'yoteyipyap', 'clicky5', 'clicky7', 'clicky9', 'clicky10']
+    const badges = ['badge', 'yoteyipyap', 'clicky5', 'clicky7', 'clicky8', 'clicky9', 'clicky10', 'clicky13', 'clicky-left', 'clicky-right']
     const rand = Math.floor(Math.random() * badges.length)
     // const coinFlip = Math.round(Math.random())
     // return `/badges/${coinFlip === 1 ? `badge` : badges[rand]}.png`
