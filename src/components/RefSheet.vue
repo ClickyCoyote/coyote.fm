5bd25d3c4900e904ee4e496423da0225bbf5bacc
Clicky Coyote <clicky@coyote.fm>
2026-05-31 13:41:44 -0500

    add primevue and ref sheet

--- src/components/RefSheet.vue
+++ src/components/RefSheet.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Image from 'primevue/image'
+import { Card } from 'primevue'
+</script>
 
 <template>
-  <img src="../assets/refsheet.png" class="w-xl">
+  <Card>
+    <template #header>
+      <i class="pi pi-download"></i>
+    </template>
+    <template #content>
+      <!--      <Image src="/refsheet.png" />-->
+      <Image alt="Image" preview>
+        <template #previewicon>
+          <i class="pi pi-search"></i>
+        </template>
+        <template #image>
+          <img src="/refsheet.png" alt="image" />
+        </template>
+        <template #preview="slotProps">
+          <img src="/refsheet.png" alt="preview" :style="slotProps.style" @click="slotProps.onClick" />
+        </template>
+      </Image>
+    </template>
+  </Card>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+
 import { createMemoryHistory, createRouter } from 'vue-router'
 import { RefSheet, SocialLinks } from '@/components'
 
@@ -16,4 +20,9 @@ const router = createRouter({
 
 createApp(App)
   .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
   .mount('#app')
